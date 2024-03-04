7316bdef5e890b1a4308fa796e04c40fec319b29
Andro Chilachava <157217297+AndroGenium@users.noreply.github.com>
2024-03-04 17:32:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const zd = "Hello World "
+
+for(let i = 1; i <= 100; i++){
+    console.log(zd + i)
+}
+
+
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(i) + "ez" + blank.repeat(diff))
+
+
+
+
+}
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(diff) + "ez" + blank.repeat(i))
+
+
+
+
+}
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(i) + "ez" + blank.repeat(diff))
+
+
+
+
+}
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(diff) + "ez" + blank.repeat(i))
+
+
+
+
+}
+
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(i) + "ez" + blank.repeat(diff))
+
+
+
+
+}
+for(i = 1; i <= 10; i++)
+{
+    let blank = " "
+    let diff = Math.abs(i-10)
+    console.log(blank.repeat(diff) + "ez" + blank.repeat(i))
+
+
+
+
+}
+
+
